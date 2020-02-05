f8cc18da5b14b4f56c3e672b7af01ce7e652e6ca
loux22 <louis.ardilly@yahoo.fr>
2020-02-05 09:43:48 +0100

    add relation

--- symfony_project/src/Entity/Groupe.php
+++ symfony_project/src/Entity/Groupe.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -30,6 +32,16 @@ class Groupe
      * @ORM\Column(type="date")
      */
     private $date;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Message", mappedBy="groupe")
+     */
+    private $messages;
+
+    public function __construct()
+    {
+        $this->messages = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -68,6 +80,37 @@ class Groupe
     public function setDate(\DateTimeInterface $date): self
     {
         $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Message[]
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages[] = $message;
+            $message->setGroupe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): self
+    {
+        if ($this->messages->contains($message)) {
+            $this->messages->removeElement($message);
+            // set the owning side to null (unless already changed)
+            if ($message->getGroupe() === $this) {
+                $message->setGroupe(null);
+            }
+        }
 
         return $this;
     }
--- symfony_project/src/Entity/Message.php
+++ symfony_project/src/Entity/Message.php
@@ -31,6 +31,16 @@ class Message
      */
     private $state;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\groupe", inversedBy="messages")
+     */
+    private $groupe;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\user", inversedBy="messages")
+     */
+    private $User;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -68,6 +78,30 @@ class Message
     public function setState(int $state): self
     {
         $this->state = $state;
+
+        return $this;
+    }
+
+    public function getGroupe(): ?groupe
+    {
+        return $this->groupe;
+    }
+
+    public function setGroupe(?groupe $groupe): self
+    {
+        $this->groupe = $groupe;
+
+        return $this;
+    }
+
+    public function getUser(): ?user
+    {
+        return $this->User;
+    }
+
+    public function setUser(?user $User): self
+    {
+        $this->User = $User;
 
         return $this;
     }
