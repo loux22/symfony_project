ac0a5a8159e7d03b6618aaae9353a4e688941db6
loux22 <louis.ardilly@yahoo.fr>
2020-02-05 11:42:29 +0100

    finish form signup

--- symfony_project/src/Form/SignupType.php
+++ symfony_project/src/Form/SignupType.php
@@ -4,19 +4,39 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class SignupType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('password')
-            ->add('email')
+            ->add('username', TextType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'le mot de passe n\'est pas confirmer.',
+                'first_options' => [
+                    'label' => 'Password',
+                    'attr' => [
+                        'placeholder' => "Password",
+
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Répeter password',
+                    'attr' => [
+                        'placeholder' => "Répeter Password",
+                    ]
+                ]
+            ])
+            ->add('email', EmailType::class)
             ->add('photo',FileType::class,array('required'=> false,))
             ->add('submit',SubmitType::class)
         ;
