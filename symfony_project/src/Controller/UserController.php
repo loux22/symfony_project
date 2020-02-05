76946eff393d4cb6cc88e57d9935740e3ac17b68
JeremYnov <jeremy.richard@ynov.com>
2020-02-05 11:35:01 +0100

    add signup form

--- symfony_project/src/Controller/UserController.php
+++ symfony_project/src/Controller/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\SignupType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -27,9 +30,26 @@ class UserController extends AbstractController
      /**
      * @Route("/signup", name="signup")
      */
-    public function signup(AuthenticationUtils $authenticationUtils)
+    public function signup(Request $request)
     {
-        $error = $authenticationUtils->getLastAuthenticationError();
-        return $this->render('user/signup.html.twig', ['error' => $error]);
+        $user = new User;
+        $form = $this->createForm(SignupType::class, $user);
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user); //ENREGISTRE L'OBJET DANS LE SYSTEME 
+
+            // $post-> fileUpload();
+
+            $manager->flush($user); //EXECUTE LA OU LES REQUETES ENREGISTREE
+
+            $this->addFlash('success', 'La création de votre compte est une réussite ' . $user->getUsername());
+            // return $this ->redirectToRoute('admin_post');
+        }
+
+        return $this->render('user/signup.html.twig', ['signupForm' => $form -> createView(),'user' => $user]);
     }
 }
