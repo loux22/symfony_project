0b0e6fa88217afe3ab8d606abd72c363050fbbc3
Léo Peyré <leo.peyre95@gmail.com>
2020-02-06 09:46:36 +0100

    message

--- symfony_project/src/Controller/GroupeController.php
+++ symfony_project/src/Controller/GroupeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class GroupeController extends AbstractController
 {
@@ -15,5 +16,39 @@ class GroupeController extends AbstractController
         return $this->render('groupe/index.html.twig', [
             'controller_name' => 'GroupeController',
         ]);
+    }
+
+    // /**
+    //  * @Route("/groupe/(id)", name="groupe")
+    //  */
+    // public function showgroup($id){
+    //     $groupe = $this -> getDoctrine()
+    //         -> getRepository('GroupeController.php')
+    //         -> find($id);
+
+    //     if(!$groupe){
+    //         throw $this -> createNotFoundException('Aucun groupe ne correspond à votre recherche ');
+    //     }
+    // }
+
+
+    /**
+     * @Route("/groupe/{id}", name="groupe")
+     */
+    public function showAllGroupOfUser($id){
+        $manager = $this -> getDoctrine() -> getManager();
+        $user = $manager -> find(User::class, $id);
+        $groupes = $user->getGroupes();
+        
+        $manager = $this->getDoctrine()->getManager();
+        $message = $manager->find(message::class, $id);
+
+
+        return $this->render('groupe/index.html.twig', [
+            "groupes" => $groupes,
+            "message" => $message
+        ]);
+
+        // return new Response(count($groupe) . "Groupe dans le repertoire");
     }
 }
